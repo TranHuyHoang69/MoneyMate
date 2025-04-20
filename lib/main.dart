d859a02b74d7cb73c5f2e3ec51806f935e33af26
TranHuyHoang69 <tranhuyhoangwork69@gmail.com>
2025-04-20 13:29:40 +0700

    thêm giao diện màn hình chính

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,68 @@
 import 'package:flutter/material.dart';
+import 'main-chiPhi.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: MyApp(), debugShowCheckedModeBanner: false);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: ThuChiPage(),
+    );
+  }
+}
+
+class ThuChiPage extends StatefulWidget {
+  @override
+  _ThuChiPageState createState() => _ThuChiPageState();
+}
+
+class _ThuChiPageState extends State<ThuChiPage>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green.shade800,
+        centerTitle: true,
+        title: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(Icons.account_balance_wallet),
+            SizedBox(width: 8),
+            Text("Tổng cộng", style: TextStyle(fontWeight: FontWeight.bold)),
+          ],
+        ),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: [Tab(text: 'Chi phí'), Tab(text: 'Thu Nhập')],
+        ),
+      ),
+
+      body: TabBarView(
+        controller: _tabController,
+        children: [ChiPhiTab(), Center(child: Text('Thu nhập'))],
+      ),
+    );
   }
 }
